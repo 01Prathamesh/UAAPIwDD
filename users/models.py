c4615dd2a912b7541063d95b3a5821e9146acc2c
Prathamesh Kasar <prathameshkasar000@gmail.com>
2024-11-14 15:23:34 +0530

    New Element : model is created with the name 'Abstractuser' in the users models.py file.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+class User(AbstractUser):
+    phone_number = models.CharField(
+        max_length=15, 
+        validators=[RegexValidator(r'^\+91\d{10}$', 'Enter a valid Indian phone number')],
+    )
+    date_of_birth = models.DateField()
+    last_login_ip = models.GenericIPAddressField(null=True, blank=True)
+
+    def __str__(self):
+        return self.username
