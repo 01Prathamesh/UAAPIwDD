ec2694ed510d2b0181473b25fd4c2bf6fcc99af4
Prathamesh Kasar <prathameshkasar000@gmail.com>
2024-11-15 11:54:06 +0530

    New Element : Database is done in postgreSQL.

--- UAAPIwDD/settings.py
+++ UAAPIwDD/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import Config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +77,17 @@ WSGI_APPLICATION = 'UAAPIwDD.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# Initialize Config to read the .env file
+config = Config()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),  # Default is localhost if not set in .env
+        'PORT': config('DB_PORT', default=5432, cast=int),  # Default to PostgreSQL port
     }
 }
 
