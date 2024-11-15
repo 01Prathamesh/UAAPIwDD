da9a2d2b5a027562d15d10b26b41b19389ff1d97
Prathamesh Kasar <prathameshkasar000@gmail.com>
2024-11-15 11:36:43 +0530

    Modification : Added structure for handling the CustomUser model.

--- users/serializers.py
+++ users/serializers.py
@@ -4,9 +4,10 @@ from rest_framework.validators import UniqueValidator
 from django.core.validators import RegexValidator
 from rest_framework_simplejwt.tokens import RefreshToken
 
+# Get the CustomUser model (which is used in place of the default User model)
 User = get_user_model()
 
-# Custom validator for Indian phone number
+# Custom validator for Indian phone numbers
 phone_number_validator = RegexValidator(
     regex=r'^\+91[6-9][0-9]{9}$|^[6-9][0-9]{9}$',
     message="Enter a valid Indian phone number"
@@ -17,17 +18,25 @@ class RegisterSerializer(serializers.ModelSerializer):
     phone_number = serializers.CharField(
         max_length=15,  # Maximum length for phone numbers with country code
         required=True,
-        validators=[phone_number_validator]
+        validators=[phone_number_validator],
+        unique=True  # Ensure the phone number is unique
     )
     
+    # To create a user, we need to include the username, password, phone number, and date of birth
     class Meta:
         model = User
         fields = ['username', 'password', 'phone_number', 'date_of_birth']
-
+    
     def create(self, validated_data):
-        # Hashing the password during user creation
+        # Creating the user with hashed password
         user = User.objects.create_user(**validated_data)
         return user
+
+    def validate_phone_number(self, value):
+        # Check if phone number is valid (through regex validator defined above)
+        if not phone_number_validator(value):
+            raise serializers.ValidationError("Enter a valid phone number.")
+        return value
 
 
 class LoginSerializer(serializers.Serializer):
